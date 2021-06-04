4a3bff26f209633f3ef7931c7e9d4adba99c0755
Tombleron <fokindacer@gmail.com>
2021-06-04 16:46:16 +0700

    First working prototype

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ use console_engine::ConsoleEngine;
 
 fn main() {
     let engine = ConsoleEngine::init_fill(100);
+
     let app = App::new(engine, 5, "wlan0".to_string());
+
     match app {
         Ok(mut  app) => { app.start().unwrap() },
         Err(_) => {},
